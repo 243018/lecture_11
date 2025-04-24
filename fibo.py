5fcb7298332ea39d61649d006fe954c52711ba21
Dominik Bachraty <dominik.ba123@gmail.com>
2025-04-24 08:48:55 +0200

    hotovo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,16 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 2) + recursive_nth_fibo(n - 1)
+
 
 def main():
-    pass
+    value = 4
+    sequence = [recursive_nth_fibo(n) for n in range(value + 1)]
+    print(sequence)
 
 
 if __name__ == "__main__":
