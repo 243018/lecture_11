66edd32f7c805dcd09532b16ac4788f3d841a305
Dominik Bachraty <dominik.ba123@gmail.com>
2025-04-24 09:12:28 +0200

    hotovo

--- flood_fill.py
+++ flood_fill.py
@@ -1,17 +1,34 @@
 import matplotlib.pyplot as plt
 
 
-def main():
-    img = plt.imread("files/img0.png")[:, :, 0]
-    # img = plt.imread("files/img1.png")[:, :, 0]
-    # img = plt.imread("files/img2.png")[:, :, 0]
+def flood_fill(img, x, y):
+    if x < 0 or x >= img.shape[0] or y < 0 or y >= img.shape[1]:
+        return img
 
-    # img = floodfill(img, 0, 0)
+    if img[x, y] == 2 or img[x, y] == 0:
+        return img
+
+    img[x, y] = 2
 
     plt.imshow(img, cmap="gray")
     plt.show(block=False)
-    plt.pause(5)
+    plt.pause(0.1)
     plt.clf()
+
+    img = flood_fill(img, x, y - 1)
+    img = flood_fill(img, x, y + 1)
+    img = flood_fill(img, x - 1, y)
+    img = flood_fill(img, x + 1, y)
+
+    return img
+
+
+def main():
+    # img = plt.imread("files/img0.png")[:, :, 0]
+    # img = plt.imread("files/img1.png")[:, :, 0]
+    img = plt.imread("files/img2.png")[:, :, 0]
+
+    img = flood_fill(img, 0, 0)
 
 
 if __name__ == "__main__":
